d9519519bf2ea25d178f74f1acdfa4534f77100b
Ach2290 <chaparro.garcia.alvaro@gmail.com>
2024-02-05 12:32:15 +0100

    Splash Screen terminado

---
+++ Interfaz/GanadariasPlus/app/src/main/java/com/example/ganadariasplus/SplashScreen.java
@@ -0,0 +1,51 @@
+package com.example.ganadariasplus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashScreen extends AppCompatActivity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        final ImageView splashImage = findViewById(R.id.Splash);
+
+        // Configurar la animación de transparencia
+        AlphaAnimation fadeIn = new AlphaAnimation(0.0f, 1.0f);
+        fadeIn.setDuration(3000); // Configurar la duración de la animación en milisegundos
+
+        fadeIn.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                splashImage.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                // La animación ha terminado, puedes iniciar la siguiente actividad aquí
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+
+        // Iniciar la animación
+        splashImage.startAnimation(fadeIn);
+    }
+}
