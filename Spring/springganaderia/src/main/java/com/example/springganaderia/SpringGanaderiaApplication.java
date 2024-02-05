4b2240493bc8f8b2b01f500656cf5f2afd5c41f7
gonzarf <gonzarf21@gmail.com>
2024-02-05 18:51:48 +0100

    Spring Initializr añadido

---
+++ Spring/springganaderia/src/main/java/com/example/springganaderia/SpringGanaderiaApplication.java
@@ -0,0 +1,13 @@
+package com.example.springganaderia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGanaderiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGanaderiaApplication.class, args);
+	}
+
+}
---
+++ Spring/springganaderia/src/test/java/com/example/springganaderia/SpringGanaderiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springganaderia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGanaderiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
