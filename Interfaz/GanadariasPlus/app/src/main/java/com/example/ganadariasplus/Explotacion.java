292a6ab334524a3473a78bab2c59e7ac668f9e35
gonzarf <gonzarf21@gmail.com>
2024-02-15 14:24:05 +0100

    activity explotacion

---
+++ Interfaz/GanadariasPlus/app/src/main/java/com/example/ganadariasplus/Explotacion.java
@@ -0,0 +1,14 @@
+package com.example.ganadariasplus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Explotacion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_explotacion);
+    }
+}
