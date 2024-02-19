246549fb922276cbabcea95361aa99f2e46f1654
gonzarf <gonzarf21@gmail.com>
2024-02-19 19:19:08 +0100

    cambios

---
+++ Interfaz/GanadariasPlus/app/src/main/java/com/example/ganadariasplus/retrofit/ApiAdapter.java
@@ -0,0 +1,34 @@
+package com.example.ganadariasplus.retrofit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiAdapter {
+    private static ApiService API_SERVICE;
+    private static final String BASE_URL = "http://192.168.1.133";
+
+    public static ApiService getApiService() {
+
+        // Creamos un interceptor y le indicamos el log level a usar
+        final HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        // Asociamos el interceptor a las peticiones
+        final OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+        httpClient.addInterceptor(logging);
+
+        if (API_SERVICE == null) {
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(httpClient.build()) // <-- set log level
+                    .build();
+
+            API_SERVICE = retrofit.create(ApiService.class);
+        }
+
+        return API_SERVICE;
+    }
+}
---
+++ Interfaz/GanadariasPlus/app/src/main/java/com/example/ganadariasplus/retrofit/ApiService.java
@@ -0,0 +1,6 @@
+package com.example.ganadariasplus.retrofit;
+
+public interface ApiService {
+
+
+}
